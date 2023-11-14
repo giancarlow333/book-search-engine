99c903743cdcad39ca4d22e2ca62d0a92e1a1e72
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-11-13 17:54:48 -0800

    Fix issue with vite.config; correct to user from users in mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -26,7 +26,7 @@ export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
-      users {
+      user {
         _id
         username
       }
