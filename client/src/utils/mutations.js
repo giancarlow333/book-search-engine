a9e1e2aea57e293b586ec76cf0087949828fee63
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-11-13 17:57:21 -0800

    Add ADD_USER mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,26 +1,17 @@
 import { gql } from '@apollo/client';
 
-/*export const ADD_PROFILE = gql`
-  mutation addProfile($name: String!, $email: String!, $password: String!) {
-    addProfile(name: $name, email: $email, password: $password) {
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
       token
-      profile {
-        _id
-        name
+      user {
+        email
+        password
+        username
       }
     }
   }
 `;
-
-export const ADD_SKILL = gql`
-  mutation addSkill($profileId: ID!, $skill: String!) {
-    addSkill(profileId: $profileId, skill: $skill) {
-      _id
-      name
-      skills
-    }
-  }
-`;*/
 
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
